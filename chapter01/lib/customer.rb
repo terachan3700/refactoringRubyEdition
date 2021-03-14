cf68cd7b03cca24daf0402a0f34df735d0e45ad3
terachan3700 <chapico01@gmail.com>
2021-03-14 17:18:31 +0900

    stateパターンの途中

--- chapter01/lib/customer.rb
+++ chapter01/lib/customer.rb
@@ -11,38 +11,38 @@ class Customer
   end
 
   def statment
-    total_amount, frequent_renter_points = 0, 0
     result = "Rental Record for #{@name}\n"
     @rentals.each do |element|
-      this_amount = 0
-
-      # 各行の金額を計算
-      case element.movie.price_code
-      when Movie::REGULAR
-        this_amount += 2
-        this_amount += (element.days_rented - 2) * 1.5 if element.days_rented > 2
-      when Movie::NEW_RELEASE
-        this_amount += 3
-      when Movie::CHILDRENS
-        this_amount += 1.5
-        this_amount += (element.days_rented - 3) * 1.5 if element.days_rented > 3
-      end
-
-      # レンタルポイントを加算
-      frequent_renter_points += 1
-      # 新作２日間レンタルでボーナス点を加算
-      if element.movie.price_code == Movie::NEW_RELEASE && element.days_rented > 1
-        frequent_renter_points += 1
-      end
-
       # このレンタルの料金を表示
-      result += "\t" + element.movie.title + "\t" + this_amount.to_s + "\n"
-      total_amount += this_amount
+      result += "\t" + element.movie.title + "\t" + element.charge.to_s + "\n"
     end
 
     # フッター業を追加
-    result += "Amount owed is #{total_amount}\n"
-    result += "You earned #{frequent_renter_points} frequent renter points"
+    result += "Amount owed is #{total_charge}\n"
+    result += "You earned #{total_frequent_renter_points} frequent renter points"
     result
+  end
+
+  def html_statment
+    result = "<h1>Rental Record for <em>#{@name}</em></h1><p>\n"
+    @rentals.each do |element|
+      # このレンタルの料金を表示
+      result += "\t" + element.movie.title + ":" + element.charge.to_s + "<br>\n"
+    end
+
+    # フッター業を追加
+    result += "<p>You owe <em>#{total_charge}</em><p>\n"
+    result += "On this rental you earned " +
+        "<em>#{total_frequent_renter_points}</em> " +
+        "frequent renter points<p>"
+    result
+  end
+
+  def total_charge
+    @rentals.inject(0) { |sum, rental| sum + rental.charge }
+  end
+
+  def total_frequent_renter_points
+    @rentals.inject(0) { |sum, rental| sum + rental.frequent_renter_points }
   end
 end
--- chapter01/lib/movie.rb
+++ chapter01/lib/movie.rb
@@ -1,3 +1,4 @@
+
 class Movie
   REGULAR = 0
   NEW_RELEASE = 1
@@ -5,8 +6,59 @@ class Movie
 
   attr_reader :title
   attr_accessor :price_code
+  attr_writer :price
 
-  def initialize(title, price_code)
-    @title, @price_code = title, price_code
+  def price_code=(value)
+    @price_code = value
+    @price = case price_code
+      when REGULAR then ReqularPrice.new
+      when NEW_RELEASE then NewReleasePrice.new
+      when CHILDRENS then ChildrensPrice.new
+    end
+  end
+
+  def initialize(title, the_price_code)
+    @title, self.price_code = title, the_price_code
+  end
+
+  def charge(days_rented)
+    @price.charge(days_rented)
+  end
+  
+  def frequent_renter_points(days_rented)
+    @price.frequent_renter_points(days_rented)
+  end
+end
+
+module DefaultPrice
+  def frequent_renter_points(days_rented)
+    1
+  end
+end
+
+class ReqularPrice
+  include DefaultPrice
+  def charge(days_rented)
+    result = 2
+    result += (days_rented - 2) * 1.5 if days_rented > 2
+    result
+  end
+end
+class NewReleasePrice
+  def charge(days_rented)
+    result = 3
+    result
+  end
+
+  def frequent_renter_points(days_rented)
+    days_rented > 1 ? 2 : 1
+  end
+end
+class ChildrensPrice
+  include DefaultPrice
+  def charge(days_rented)
+    result = 1.5
+    result += (days_rented - 3) * 1.5 if days_rented > 3
+    result
   end
 end
